a1d9168ce384022635880dfbb2c1392a8323e248
Mariel <111444526+MarielML@users.noreply.github.com>
2023-04-21 15:22:54 -0300

    Create Grupo.kt

---
+++ app/src/main/java/ar/edu/unlam/organizador/entidades/Grupo.kt
@@ -0,0 +1,8 @@
+package com.example.organizadordetareas.entidades
+
+data class Grupo (
+  val nombre: String
+  val ambiente: Ambientes
+  val cantidadDeParticipantes: Int
+  val contrasenia: String
+)

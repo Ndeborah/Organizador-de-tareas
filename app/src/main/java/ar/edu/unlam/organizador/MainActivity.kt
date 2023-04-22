141e0191a8be14a236747e723d50c6b55279c1a9
Mariel <111444526+MarielML@users.noreply.github.com>
2023-04-22 12:17:44 -0300

    Update MainActivity.kt

--- app/src/main/java/ar/edu/unlam/organizador/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/organizador/MainActivity.kt
@@ -1,45 +1,67 @@
 package ar.edu.unlam.organizador
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ar.edu.unlam.organizador.ui.theme.OrganizadorTheme
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ar.edu.unlam.organizador.databinding.ActivityMainBinding
+import com.example.organizadordetareas.repositorios.UsuarioRepositorio
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContent {
-            OrganizadorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        // Botón entrar con usuario anónimo: UsuarioRepositorio.iniciar("Anónimo", "")
+
+        binding.buttonIngresar.setOnClickListener {
+            val usuario = binding.etUsuario.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if(validarImput()) {
+                if(UsuarioRepositorio.existe(usuario, password)) {
+                    ingresar(usuario, password)
+                    finish()
+                } else {
+                    Toast.makeText(this, this.getString(R.string.datosIncorrectos), Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                Toast.makeText(this, this.getString(R.string.ingresoFail), Toast.LENGTH_SHORT).show()
             }
         }
-        
-        // Botón entrar con usuario anónimo: UsuarioRepositorio.iniciar("Anónimo", "")
+
+        binding.buttonCrearUsuario.setOnClickListener {
+            crearUsuario()
+            onPause()
+        }
+
+        binding.buttonAnonimo.setOnClickListener {
+            ingresarAnonimo()
+        }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+    private fun ingresar(usuario: String, password: String) {
+        val intent = Intent(this, IngresarAGrupoActivity::class.java).apply {
+            putExtra("usuario", usuario)
+            putExtra("password", password)
+        }
+        startActivity(intent)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OrganizadorTheme {
-        Greeting("Android")
+    private fun crearUsuario() {
+        val intent = Intent(this, CrearUsuarioActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun validarImput(): Boolean {
+        return binding.etUsuario.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()
+    }
+
+    private fun ingresarAnonimo() {
+        ingresar("Anonimo", "")
     }
 }

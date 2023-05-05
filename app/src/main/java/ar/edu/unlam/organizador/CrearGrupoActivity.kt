e12ac718cdfe88c1ccb453dd9800a4e2080adb90
MarielML <marielmarmol1511@outlook.com>
2023-05-05 08:40:55 -0300

    Crear grupo

--- app/src/main/java/ar/edu/unlam/organizador/CrearGrupoActivity.kt
+++ app/src/main/java/ar/edu/unlam/organizador/CrearGrupoActivity.kt
@@ -8,20 +8,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ar.edu.unlam.organizador.entidades.Grupo
 import ar.edu.unlam.organizador.repositorios.GrupoRepositorio
@@ -29,7 +34,10 @@ import ar.edu.unlam.organizador.ui.theme.OrganizadorTheme
 import ar.edu.unlam.organizador.ui.theme.Pink80
 import ar.edu.unlam.organizador.ui.theme.Purple40
 
-class MainActivity : ComponentActivity() {
+class CrearGrupoActivity : ComponentActivity() {
+    private lateinit var nuevoGrupo: Grupo
+    private lateinit var nombre: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -48,8 +56,7 @@ class MainActivity : ComponentActivity() {
         ) {
             Column {
                 Menu()
-                MostrarGrupos(GrupoRepositorio.grupos)
-                Botones()
+                CrearGrupo()
             }
         }
     }
@@ -87,20 +94,9 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    private fun MostrarGrupos(datos: MutableList<Grupo>) {
-        LazyColumn(
-            contentPadding = PaddingValues(10.dp),
-            verticalArrangement = Arrangement.spacedBy(5.dp)
-        ) {
-            items(datos) {
-                item -> ListItemRow(item)
-            }
-        }
-    }
-
-    @Composable
-    private fun ListItemRow(item: Grupo) {
+    private fun CrearGrupo() {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -108,34 +104,32 @@ class MainActivity : ComponentActivity() {
                 .padding(horizontal = 10.dp, vertical = 10.dp)
         ) {
             Column() {
-                Text(text = item.nombre)
-                Text(text = "Pendientes: ")
+                Text(text = "Crear Grupo")
             }
         }
-    }
-
-    @Composable
-    private fun Botones() {
-        Column (
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.SpaceAround,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Spacer(modifier = Modifier.size(100.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(horizontal = 10.dp, vertical = 10.dp)
         ) {
-            Button(onClick = {
-                irACrearGrupo()
-                onStop()
-            }) {
-                Text("Crear Grupo", color = Color.White)
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text("Unirse a un Grupo", color = Color.White)
+            Column() {
+                nombre = TextField (
+                    value = " ",
+                    onValueChange = {},
+                    label = {Text(text = "Ingresar el nombre del nuevo Grupo")}
+                ).toString()
             }
         }
-    }
-
-    private fun irACrearGrupo() {
-        val intent = Intent(this, CrearGrupoActivity::class.java)
-        startActivity(intent)
+        Spacer(modifier = Modifier.size(100.dp))
+        Button(onClick = {
+            nuevoGrupo = Grupo(nombre, 0, "")
+            GrupoRepositorio.agregar(nuevoGrupo)
+            finish()
+        }) {
+            Text(text = "Crear")
+        }
     }
 
     private fun irAChat() {

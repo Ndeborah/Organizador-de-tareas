5817853455ff4c9fbde8daf0c903c88dca634ef3
MarielML <marielmarmol1511@outlook.com>
2023-05-05 18:53:33 -0300

    Ingreso a grupo

--- app/src/main/java/ar/edu/unlam/organizador/GrupoActivity.kt
+++ app/src/main/java/ar/edu/unlam/organizador/GrupoActivity.kt
@@ -3,14 +3,7 @@ package ar.edu.unlam.organizador
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import ar.edu.unlam.organizador.entidades.Grupo
 import ar.edu.unlam.organizador.repositorios.GrupoRepositorio
 import ar.edu.unlam.organizador.ui.theme.OrganizadorTheme

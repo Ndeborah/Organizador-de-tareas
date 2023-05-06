ff078de61589827e8db1d366948de5d3a8f38589
MarielML <marielmarmol1511@outlook.com>
2023-05-06 12:47:32 -0300

    Nombre de grupo

--- app/src/main/java/ar/edu/unlam/organizador/GrupoActivity.kt
+++ app/src/main/java/ar/edu/unlam/organizador/GrupoActivity.kt
@@ -1,14 +1,33 @@
 package ar.edu.unlam.organizador
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import ar.edu.unlam.organizador.entidades.Grupo
 import ar.edu.unlam.organizador.repositorios.GrupoRepositorio
 import ar.edu.unlam.organizador.ui.theme.OrganizadorTheme
+import ar.edu.unlam.organizador.ui.theme.Pink80
+import ar.edu.unlam.organizador.ui.theme.Purple40
 
 class GrupoActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,8 +37,79 @@ class GrupoActivity : ComponentActivity() {
 
         setContent {
             OrganizadorTheme {
-                Text(grupo.nombre)
+                Base(grupo)
             }
         }
+    }
+
+    @Composable
+    private fun Base(grupo: Grupo) {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.Black
+        ) {
+            Column {
+                Menu()
+                NombreDeGrupo(grupo)
+            }
+        }
+    }
+
+    @Composable
+    private fun Menu() {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Purple40),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {
+                    irAChat()
+                    onStop()
+                }
+            ) {
+                Text(text = "Chat")
+            }
+            Button(
+                onClick = {}
+            ) {
+                Text(text = "Grupos", color = Pink80)
+            }
+            Button(
+                onClick = {
+                    irATareas()
+                    onStop()
+                }
+            ) {
+                Text(text = "Tareas")
+            }
+        }
+    }
+
+    @Composable
+    private fun NombreDeGrupo(grupo: Grupo) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(horizontal = 10.dp, vertical = 10.dp)
+        ) {
+            Column() {
+                Text(text = grupo.nombre)
+            }
+        }
+    }
+
+    private fun irAChat() {
+        val intent = Intent(this, ChatActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun irATareas() {
+        val intent = Intent(this, TareasActivity::class.java)
+        startActivity(intent)
     }
 }
